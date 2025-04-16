d9e8b394a8f6e3b4baec05b9c8152587e3fdf075
Karl Magnus Leonardo Nøkling <114525120+Korny94@users.noreply.github.com>
2025-04-16 09:42:23 +0200

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("downloadBtn").addEventListener("click", () => {
+  // Endre denne URL-en til din faktiske .exe eller .zip nedlastingslenke
+  window.location.href =
+    "https://github.com/Helse-Tech/downloadCopycat/releases/download/setup/Copycat.Setup.1.0.0.exe";
+});
